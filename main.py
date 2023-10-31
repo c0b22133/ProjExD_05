a2fa365b7dca32aa7f0924a20fccf36a354c9505
Hirotada Fujita <c0b2212931@edu.teu.ac.jp>
2023-10-31 18:12:44 +0900

    shield実装

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 import sys
 import pygame as pg
+import time
+import random
+
+
+class Tank:
+    def __init__(self, x,y, image_path):
+        self.x = x
+        self.y = y
+        self.image = pg.image.load(image_path)
+        # self.rect = self.image.get_rect()
+        # self.rect.center = x, y
+
+    def move_left(self, distance, min_x):
+        self.x -= distance
+        if self.x < min_x:
+            self.x = min_x
+
+    def move_right(self, distance, max_x):
+        self.x += distance
+        if self.x > max_x - self.image.get_width():
+            self.x = max_x - self.image.get_width()
+
+    def draw(self, screen):
+        screen.blit(self.image, [self.x, self.y])
+
+class Shield(pg.sprite.Sprite):
+    """
+    tabキーを押すと重力バリアが発動する
+    """
+    def __init__(self, tank:Tank,  size: int ,life: int):
+        super().__init__()
+        self.tank = tank
+        self.image = pg.Surface((2*size, 2*size))
+        self.image.set_alpha(50)
+        pg.draw.circle(self.image, (1, 0, 0), (size, size), size)
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.tank.x + self.tank.image.get_width() / 2
+        self.rect.centery = self.tank.y
+        self.life = life
+
+    def update(self):
+        """
+        発動してからlifeがゼロになるまで発動し、ゼロになったらkillされる
+        """
+        self.rect.centerx = self.tank.x + self.tank.image.get_width() / 2
+        self.rect.centery = self.tank.y
+        self.life -= 1
+        if self.life < 0:
+            self.kill()
+
 
 def main():
     pg.display.set_caption("弾幕ゲー")
@@ -7,11 +58,11 @@ def main():
     clock = pg.time.Clock()
 
     bg_img = pg.image.load("ex05/fig/background.png")
-    tank_img = pg.image.load("ex05/fig/player1.gif")
+    tank = Tank(300, 500, "ex05/fig/player1.gif")
+    sheild = pg.sprite.Group()
 
-    tmr = 0
-    x = 300  # タンクの初期 x 座標
-    y = 500  # タンクの初期 y 座標
+    obstacles = []
+    next_obstacle_time = time.time() + random.randint(1, 3)
 
     while True:
         for event in pg.event.get():
@@ -20,20 +71,22 @@ def main():
 
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
-            x -= 10  # 左に移動
-            if x < 0:  # 画面の左端を超えないように
-                x = 0
+            tank.move_left(10, 0)
         if keys[pg.K_d]:
-            x += 10  # 右に移動
-            if x > 600 - tank_img.get_width():
-                x = 600 - tank_img.get_width()
+            tank.move_right(10, 600)
+
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_TAB :
+                    sheild.add(Shield(tank, 100, 300))
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(tank_img, [x, y])
+        tank.draw(screen)
 
+        sheild.update()
+        sheild.draw(screen)
         pg.display.update()
-        tmr += 1
         clock.tick(100)
+
 
 if __name__ == "__main__":
     pg.init()
